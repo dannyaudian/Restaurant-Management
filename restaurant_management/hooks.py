29c55aee88657988b1e64a89fcef840470b0c563
dannyaudian <danny.a.pratama@gmail.com>
2025-04-12 20:48:14 +0700

    gkkj

--- restaurant_management/hooks.py
+++ restaurant_management/hooks.py
@@ -15,7 +15,7 @@ app_license = "MIT"
 # app_include_js = "/assets/restaurant_management/js/restaurant_management.js"
 
 # include js, css files in header of web template
-# web_include_css = "/assets/restaurant_management/css/restaurant_management.css"
+web_include_css = "/assets/restaurant_management/css/dummy.css"
 web_include_js = [
     "/assets/restaurant_management/js/waiter_order.js",
 ]
