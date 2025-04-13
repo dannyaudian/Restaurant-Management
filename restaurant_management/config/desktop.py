180400c6f5255b5965392a033334c3473cea96f3
dannyaudian <danny.a.pratama@gmail.com>
2025-04-14 00:45:56 +0700

    desktop

--- restaurant_management/config/desktop.py
+++ restaurant_management/config/desktop.py
@@ -1,12 +1,13 @@
 from frappe import _
 
 def get_data():
-	return [
-		{
-			"module_name": "Restaurant Management",
-			"color": "grey",
-			"icon": "octicon octicon-file-directory",
-			"type": "module",
-			"label": _("Restaurant Management")
-		}
-	]
+    return [
+        {
+            "module_name": "Restaurant Management",
+            "type": "module",
+            "label": _("Restaurant Management"),
+            "color": "orange",
+            "icon": "octicon octicon-utensils",  # pakai icon apapun yang kamu mau
+            "is_standard": 1,
+        }
+    ]
