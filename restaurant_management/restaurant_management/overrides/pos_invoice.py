7a79da880a5916f7b204261528ada8379c311eeb
dannyaudian <danny.a.pratama@gmail.com>
2025-04-13 20:26:30 +0700

    fix error override py

--- restaurant_management/restaurant_management/overrides/pos_invoice.py
+++ restaurant_management/restaurant_management/overrides/pos_invoice.py
@@ -7,7 +7,7 @@ class RestaurantPOSInvoice(POSInvoice):
         super(RestaurantPOSInvoice, self).validate()
         self.validate_restaurant_fields()
         self.set_branch_from_waiter_order()
-    self.validate_branch_permission()
+        self.validate_branch_permission()
     
     def validate_restaurant_fields(self):
         # If this is linked to a restaurant table, ensure waiter order is valid
@@ -16,12 +16,19 @@ class RestaurantPOSInvoice(POSInvoice):
             waiter_order_table = frappe.db.get_value("Waiter Order", self.restaurant_waiter_order, "table")
             if waiter_order_table != self.restaurant_table:
                 frappe.throw(_("The Waiter Order is not associated with the selected Table"))
-
+    
+    def validate_branch_permission(self):
+        """Validate that user has permission to access this branch"""
+        if self.branch_code:
+            from restaurant_management.restaurant_management.utils.branch_permissions import user_has_branch_access
+            if not user_has_branch_access(self.branch_code):
+                frappe.throw(_("You don't have permission to access branch {0}").format(self.branch_code))
+    
     def set_branch_from_waiter_order(self):
         """Set branch and branch_code from Waiter Order"""
         if self.restaurant_waiter_order and not self.branch:
             waiter_order = frappe.get_doc("Waiter Order", self.restaurant_waiter_order)
-
+            
             # Get branch from waiter order's table
             if waiter_order.table:
                 table = frappe.get_doc("Table", waiter_order.table)
@@ -33,29 +40,22 @@ class RestaurantPOSInvoice(POSInvoice):
                         fields=["name"],
                         limit=1
                     )
-
+                    
                     if branch:
                         self.branch = branch[0].name
                         self.branch_code = table.branch_code
                     else:
                         # Set just branch code if branch not found
                         self.branch_code = table.branch_code
-
-def validate_branch_permission(self):
-    """Validate that user has permission to access this branch"""
-    if self.branch_code:
-        from restaurant_management.restaurant_management.utils.branch_permissions import user_has_branch_access
-        if not user_has_branch_access(self.branch_code):
-            frappe.throw(_("You don't have permission to access branch {0}").format(self.branch_code))
-
+    
     def on_submit(self):
         super(RestaurantPOSInvoice, self).on_submit()
         self.update_restaurant_status()
-
+        
         # Check if we need to automatically create Sales Order
         if self.restaurant_waiter_order:
             auto_create = frappe.db.get_value("POS Profile", self.pos_profile, "auto_create_sales_order")
-
+            
             if auto_create:
                 from restaurant_management.api.pos_restaurant import create_sales_order_from_waiter_order
                 create_sales_order_from_waiter_order(self.restaurant_waiter_order, self.name)
@@ -104,30 +104,3 @@ class CustomPOSInvoice(POSInvoice):
             self.naming_series = self.naming_series.replace("{branch_code}", self.branch_code)
         # Call parent autoname
         super().autoname()
-
-def link_to_sales_order(doc, method=None):
-    """Link POS Invoice to Sales Order on submission"""
-    if doc.docstatus != 1 or not doc.restaurant_waiter_order:
-        return
-
-    # Check if we need to create a Sales Order
-    auto_create = frappe.db.get_value("POS Profile", doc.pos_profile, "auto_create_sales_order")
-
-    if auto_create:
-        # Try to find existing Sales Order
-        sales_order = frappe.db.get_value("Sales Order",
-            {"restaurant_waiter_order": doc.restaurant_waiter_order, "docstatus": ["<", 2]},
-            "name")
-
-        if sales_order:
-            # Link to existing Sales Order if one exists
-            if not doc.sales_order:
-                doc.db_set("sales_order", sales_order)
-        else:
-            # Create a new Sales Order
-            from restaurant_management.api.pos_restaurant import create_sales_order_from_waiter_order
-            result = create_sales_order_from_waiter_order(doc.restaurant_waiter_order, doc.name)
-
-            if result.get("success") and result.get("sales_order"):
-                if not doc.sales_order:
-                    doc.db_set("sales_order", result.get("sales_order"))
